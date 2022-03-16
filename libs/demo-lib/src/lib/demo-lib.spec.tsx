08ff3e2b0ec5cdae5c870143a068a8825b342849
Sebastien Dubois <seb@dsebastien.net>
2022-03-16 21:33:19 +0100

    Added demo lib

---
+++ libs/demo-lib/src/lib/demo-lib.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import DemoLib from './demo-lib';
+
+describe('DemoLib', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<DemoLib />);
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ libs/demo-lib/src/lib/demo-lib.tsx
@@ -0,0 +1,13 @@
+
+/* eslint-disable-next-line */
+export interface DemoLibProps {}
+
+export function DemoLib(props: DemoLibProps) {
+  return (
+    <div>
+      <h1>Welcome to DemoLib!</h1>
+    </div>
+  );
+}
+
+export default DemoLib;
